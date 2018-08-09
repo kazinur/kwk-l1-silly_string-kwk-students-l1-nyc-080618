b9cb2641e79d4d5423f6e7f90d991663610e509a
kazinur <kazinur@mybihs.org>
2018-08-09 04:00:09 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -8,12 +8,12 @@ your_favorite_word = "Oh_my_god".upcase
 end 
 puts "MY_FAVORITE_WORD_IN_ALL_CAPS_IS # {"your_favorite_word"}"
  
-
+def lowercased_phrase
 lowercased_phrase = "IM_NOT_SHOUTING".downcase  
 end 
 puts "I_was_like # {"lowercased_phrase"}" 
   
-
+def big_word
 big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word.size
@@ -22,7 +22,7 @@ puts "There are # {"letter_count"} in # {"big_word"}"
  
   
 
-
+def bigger_word
 bigger_word = "supercalifragilisticexpialidocious"+"wow"+"mom"
 
 new_letter_count = bigger_word.size
@@ -30,12 +30,13 @@ end
 puts "There are now # {"new_letter_count"} in # {"bigger_word"}"
  
 
-
+def proper_sentence
 proper_sentence = "i really like programming".capitalize
 end 
 puts "# {"proper_sentence"}"
   
 
+def sentence 
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" 
 end 
 puts "# {"sentence"}".swapcase 
