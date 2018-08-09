7b85b19b3dd0798b297170c865c1e3e0df40f25b
kazinur <kazinur@mybihs.org>
2018-08-09 03:55:19 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,7 +1,7 @@
 def your_favorite_word
 your_favorite_word = "Oh_my_god!" 
 end 
-puts "my_favorite_word_is # {"your_favorite_word"}" 
+puts "My favorite word is # {"your_favorite_word"}" 
  
 
 your_favorite_word = "Oh_my_god".upcase  
