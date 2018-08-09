15d9e7dc44a023cbe60025a2e94af85db7dac1e8
kazinur <kazinur@mybihs.org>
2018-08-09 04:03:02 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -6,19 +6,19 @@ puts "My favorite word is #{"your_favorite_word"}"
 def your_favorite_word
 your_favorite_word = "Oh_my_god".upcase  
 end 
-puts "MY_FAVORITE_WORD_IN_ALL_CAPS_IS # {"your_favorite_word"}"
+puts "MY_FAVORITE_WORD_IN_ALL_CAPS_IS #{"your_favorite_word"}"
  
 def lowercased_phrase
 lowercased_phrase = "IM_NOT_SHOUTING".downcase  
 end 
-puts "I_was_like # {"lowercased_phrase"}" 
+puts "I_was_like #{"lowercased_phrase"}" 
   
 def big_word
 big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word.size
 end 
-puts "There are # {"letter_count"} in # {"big_word"}"
+puts "There are #{"letter_count"} in #{"big_word"}"
  
   
 
@@ -27,16 +27,16 @@ bigger_word = "supercalifragilisticexpialidocious"+"wow"+"mom"
 
 new_letter_count = bigger_word.size
 end 
-puts "There are now # {"new_letter_count"} in # {"bigger_word"}"
+puts "There are now #{"new_letter_count"} in #{"bigger_word"}"
  
 
 def proper_sentence
 proper_sentence = "i really like programming".capitalize
 end 
-puts "# {"proper_sentence"}"
+puts "#{"proper_sentence"}"
   
 
 def sentence 
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" 
 end 
-puts "# {"sentence"}".swapcase 
+puts "#{"sentence"}".swapcase 
