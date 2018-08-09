e5dd576754072e4b455a4b705a99865792e734ba
kazinur <kazinur@mybihs.org>
2018-08-09 03:51:55 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,46 +1,41 @@
-# Challenge 1: Fill this string in with your favorite word
+def your_favorite_word
+your_favorite_word = "Oh_my_god!" 
+end 
+puts "my_favorite_word_is # {"your_favorite_word"}" 
+ 
 
-your_favorite_word = "oh_my_god!" 
-puts "my_favorite_word_is #{your_favorite_word}"
+your_favorite_word = "Oh_my_god".upcase  
+end 
+puts "MY_FAVORITE_WORD_IN_ALL_CAPS_IS # {"your_favorite_word"}"
+ 
 
-# Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
-
-favorite_word_in_all_caps = "Oh_my_god".upcase  
-
-puts "MY FAVORITE WORD IN ALL CAPS IS #{favorite_word_in_all_caps}"
-
-# Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
-
-lowercased_phrase = "i_m_not_shouting".downcase  
-
-puts "i_was_like 
-#{lowercased_phrase}" 
-
-
-# Challenge 4: Count how many letters are in that big_word
+lowercased_phrase = "IM_NOT_SHOUTING".downcase  
+end 
+puts "I_was_like # {"lowercased_phrase"}" 
+  
 
 big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word.size
+end 
+puts "There are # {"letter_count"} in # {"big_word"}"
+ 
+  
 
-puts "There are #{letter_count} in #{big_word}"
-
-# Challenge 5: Add "wow" and "mom" to big_word
 
 bigger_word = "supercalifragilisticexpialidocious"+"wow"+"mom"
 
 new_letter_count = bigger_word.size
+end 
+puts "There are now # {"new_letter_count"} in # {"bigger_word"}"
+ 
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
-
-# Challenge 6: Capitalize this sentence properly
 
 proper_sentence = "i really like programming".capitalize
+end 
+puts "# {"proper_sentence"}"
+  
 
-puts "#{proper_sentence}"
-
-# Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".swapcase 
-
-
-puts "#{sentence}"
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" 
+end 
+puts "# {"sentence"}".swapcase 
